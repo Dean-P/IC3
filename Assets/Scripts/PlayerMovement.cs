a6baea3b6c9bc22f838b0f36f860055301137cc6
Dean <D_S_Preston@hotmail.com>
2021-03-04 17:21:55 +1100

    Added project folders

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController controller = null;
+
+    [SerializeField]
+    float speed = 1f;
+
+    [SerializeField]
+    const float walkSpeed = 2f;
+    [SerializeField]
+    const float runSpeed = 4f;
+    [SerializeField]
+    const float crawlSpeed = 1f;
+
+    // bool walking = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+        }
+        else if(Input.GetKey(KeyCode.LeftControl))
+        {
+            speed = crawlSpeed;
+        }
+        else
+        {
+            speed = walkSpeed;
+        }
+
+        // -1 and 1
+        float hori = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        controller.Move(new Vector3(hori,0,vert) * Time.deltaTime * speed);
+    }
+}
